4f44e7ac0da34da8bf2e55004d851164be438e4b
Simon Foster <simon.foster@york.ac.uk>
2022-07-22 11:23:43 +0100

    Fixed core and started work on healthiness conditions

--- utp_rea_core.thy
+++ utp_rea_core.thy
@@ -8,35 +8,36 @@ alphabet 'e rea_vars = des_vars +
 
 type_synonym ('t, '\<alpha>) rp = "('t, '\<alpha>) rea_vars_scheme"
 
-type_synonym ('t,'\<alpha>,'\<beta>) rel_rp  = "('t,'\<alpha>) rea_vars_scheme \<leftrightarrow> ('t,'\<beta>) rea_vars_scheme"
+type_synonym ('t,'\<alpha>,'\<beta>) rel_rp  = "(('t,'\<alpha>) rea_vars_scheme, ('t,'\<beta>) rea_vars_scheme) urel"
 
-type_synonym ('t, '\<alpha>) hrel_rp = "('t,'\<alpha>) rea_vars_scheme \<leftrightarrow> ('t,'\<alpha>) rea_vars_scheme"
+type_synonym ('t, '\<alpha>) hrel_rp = "(('t,'\<alpha>) rea_vars_scheme, ('t,'\<alpha>) rea_vars_scheme) urel"
 
 translations
   (type) "('t,'\<alpha>) rp" <= (type) "('t, '\<alpha>) rea_vars_scheme"
   (type) "('t,'\<alpha>) rp" <= (type) "('t, '\<alpha>) rea_vars_ext"
-  (type) "('t,'\<alpha>,'\<beta>) rel_rp" <= (type) "('t,'\<alpha>) rea_vars_scheme \<leftrightarrow> ('\<gamma>,'\<beta>) rea_vars_scheme"
-  (type) "('t,'\<alpha>) hrel_rp"  <= (type) "('t,'\<alpha>) rea_vars_scheme \<leftrightarrow> ('\<gamma>,'\<beta>) rea_vars_scheme"
+  (type) "('t,'\<alpha>,'\<beta>) rel_rp" <= (type) "(('t,'\<alpha>) rea_vars_scheme, ('\<gamma>,'\<beta>) rea_vars_scheme) urel"
+  (type) "('t,'\<alpha>) hrel_rp"  <= (type) "(('t,'\<alpha>) rea_vars_scheme, ('\<gamma>,'\<beta>) rea_vars_scheme) urel"
 
-notation rea_vars.more\<^sub>L ("\<Sigma>\<^sub>R")
+notation rea_vars.more\<^sub>L ("\<^bold>v\<^sub>R")
+
+term "\<^bold>v\<^sub>R"
+
 
 syntax
-  "_svid_rea_alpha"  :: "svid" ("\<Sigma>\<^sub>R")
+  "_svid_rea_alpha"  :: "svid" ("\<^bold>v\<^sub>R")
 
 translations
   "_svid_rea_alpha" => "CONST rea_vars.more\<^sub>L"
 
-declare des_vars.splits [alpha_splits del]
-declare des_vars.splits [alpha_splits]
 (*declare zero_list_def [upred_defs]
 declare plus_list_def [upred_defs]*)
 declare prefixE [elim]
 
 abbreviation wait_f::"('t, '\<alpha>, '\<beta>) rel_rp \<Rightarrow> ('t, '\<alpha>, '\<beta>) rel_rp" ("_\<^sub>f" [1000] 1000)
-where "wait_f R \<equiv> R\<lbrakk>false/wait\<^sup><\<rbrakk>"
+where "wait_f R \<equiv> R\<lbrakk>False/wait\<^sup><\<rbrakk>"
 
 abbreviation wait_t::"('t, '\<alpha>, '\<beta>) rel_rp \<Rightarrow> ('t, '\<alpha>, '\<beta>) rel_rp" ("_\<^sub>t" [1000] 1000)
-  where "wait_t R \<equiv> R\<lbrakk>true/wait\<^sup><\<rbrakk>"
+  where "wait_t R \<equiv> R\<lbrakk>True/wait\<^sup><\<rbrakk>"
 
 (*
 syntax
@@ -50,15 +51,16 @@ translations
   "P \<^sub>t" \<rightleftharpoons> "CONST usubst (CONST subst_upd id\<^sub>s (CONST in_var CONST wait) true) P"
 *)
 
-definition lift_rea :: "('\<alpha> \<leftrightarrow>'\<beta>) \<Rightarrow> ('t, '\<alpha>, '\<beta>) rel_rp" ("\<lceil>_\<rceil>\<^sub>R") where
-"\<lceil>P\<rceil>\<^sub>R \<equiv> P \<up> (\<Sigma>\<^sub>R\<^sup>2)"
-term "\<lceil>P\<rceil>\<^sub>R"
+(*
+abbreviation lift_rea :: "('\<alpha>, '\<beta>) urel \<Rightarrow> ('t, '\<alpha>, '\<beta>) rel_rp" ("\<lceil>_\<rceil>\<^sub>R") where
+"\<lceil>P\<rceil>\<^sub>R \<equiv> P \<up> (\<^bold>v\<^sub>R\<^sup>2)"
 
-definition drop_rea :: "('t, '\<alpha>, '\<beta>) rel_rp \<Rightarrow> ('\<alpha> \<leftrightarrow>'\<beta>)" ("\<lfloor>_\<rfloor>\<^sub>R") where
-"\<lfloor>P\<rfloor>\<^sub>R \<equiv> P \<down> (\<Sigma>\<^sub>R\<^sup>2)"
-term "\<lfloor>P\<rfloor>\<^sub>R"
 
-abbreviation rea_pre_lift :: "_ \<Rightarrow> _" ("\<lceil>_\<rceil>\<^sub>R\<^sub><") where "\<lceil>n\<rceil>\<^sub>R\<^sub>< \<equiv> \<lceil>n\<^sup><\<rceil>\<^sub>R"
+definition drop_rea :: "('t, '\<alpha>, '\<beta>) rel_rp \<Rightarrow> ('\<alpha>, '\<beta>) urel" ("\<lfloor>_\<rfloor>\<^sub>R") where
+"\<lfloor>P\<rfloor>\<^sub>R \<equiv> P \<down> (\<^bold>v\<^sub>R\<^sup>2)"
+
+
+abbreviation rea_pre_lift :: "('d \<Rightarrow> 'e) \<Rightarrow> ('a, 'b, 'c) rel_rp"  ("\<lceil>_\<rceil>\<^sub>R\<^sub><") where "\<lceil>n\<rceil>\<^sub>R\<^sub>< \<equiv> \<lceil>n\<^sup><\<rceil>\<^sub>R"
 
 
 lemma unrest_ok_lift_rea [unrest]:
@@ -90,3 +92,6 @@ translations
   "_svid_itrace" == "CONST itrace"
   "iter[n](P)"   == "CONST uop (CONST tr_iter n) P"
   
+*)
+
+end
