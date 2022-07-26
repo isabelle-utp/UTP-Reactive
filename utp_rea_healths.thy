847e6e3043150ee7865c6ceb87b4923e7c7d2384
Thomas Wright <tom.tdw@gmail.com>
2022-07-26 16:46:04 +0100

    Most of the remaining R1 healthiness condition lemmas.

--- utp_rea_healths.thy
+++ utp_rea_healths.thy
@@ -28,8 +28,16 @@ lemma R1_Continuous: "Continuous R1"
   by (simp add: Continuous_def, pred_auto)
 
 (*
-lemma R1_unrest: "\<lbrakk> x \<bowtie> in_var tr; x \<bowtie> out_var tr; x \<sharp> P \<rbrakk> \<Longrightarrow> x \<sharp> R1(P)"
-  by (simp add: R1_def unrest lens_indep_sym)
+lemma R1_unrest:
+  assumes "x \<bowtie> (tr ;\<^sub>L fst\<^sub>L)" "x \<bowtie> (tr ;\<^sub>L snd\<^sub>L)" "unrest ($x) P"
+  shows "unrest ($x) (R1 P)"
+proof -
+  assume "mwb_lens x"
+  have 1: "unrest ($x) (tr\<^sup><)\<^sub>e"
+    using assms(1) apply(unrest)
+    oops
+    
+qed
 *)
 
 lemma R1_false: "R1(false) = false"
@@ -72,11 +80,6 @@ lemma R1_inf: "R1(P \<sqinter> Q) = (R1(P) \<sqinter> R1(Q))"
 lemma R1_USUP:
   "R1 (\<Sqinter>i \<in> A. P i) = (\<Sqinter> i \<in> A. R1 (P i))"
   by (simp add: fun_eq_iff R1_def)
-
-(* Worse version without lattice expressions *)
-lemma R1_USUP':
-  "R1(SEXP(\<lambda> s. \<Sqinter> i. (i \<in> A \<and> P(i)(s)))) = SEXP(\<lambda> s. \<Sqinter> i. (i \<in> A \<and> (R1(P(i)))(s)))"
-  by (simp add: fun_eq_iff R1_def; pred_auto)
 
 lemma R1_Sup [closure]: "\<lbrakk> \<And> P. P \<in> A \<Longrightarrow> P is R1; A \<noteq> {} \<rbrakk> \<Longrightarrow> \<Sqinter> A is R1"
   using R1_Continuous by (auto simp add: Continuous_def Healthy_def)
@@ -156,28 +159,116 @@ lemma skip_is_R1 [closure]: "II is R1"
 text \<open> If a variable is unrestricted in a substitution then it's application has no effect. \<close>
 
 lemma usubst_apply_unrest:
-  "\<lbrakk> vwb_lens x; unrest_usubst \<lbrakk>x\<rbrakk>\<^sub>\<sim> \<sigma> \<rbrakk> \<Longrightarrow> \<langle>\<sigma>\<rangle>\<^sub>s x = var x"
+  "\<lbrakk> vwb_lens x; $x \<sharp>\<^sub>s \<sigma> \<rbrakk> \<Longrightarrow> \<langle>\<sigma>\<rangle>\<^sub>s x = var x"
 proof -
-  assume 1: "vwb_lens x" and "unrest_usubst \<lbrakk>x\<rbrakk>\<^sub>\<sim> \<sigma>"
+  assume 1: "vwb_lens x" and "$x \<sharp>\<^sub>s \<sigma>"
   hence "\<forall>s s'. \<sigma> (s \<oplus>\<^sub>L s' on x) = \<sigma> s \<oplus>\<^sub>L s' on x"
-    by (metis lens_scene_override unrest_usubst_def vwb_lens_mwb)
-  thus "\<langle>\<sigma>\<rangle>\<^sub>s x = get\<^bsub>x\<^esub>"
-    by (metis 1 fun_eq_iff lens_override_def lens_override_idem mwb_lens_weak subst_lookup_def vwb_lens_mwb weak_lens.put_get)
+    by (simp add: unrest_usubst_def)
+  thus "\<langle>\<sigma>\<rangle>\<^sub>s x = var x"
+    by (metis 1 lens_override_def lens_override_idem mwb_lens_weak subst_lookup_def vwb_lens_mwb weak_lens.put_get)
 qed
 
-lemma subst_R1: "\<lbrakk> (unrest_usubst \<lbrakk>tr ;\<^sub>L fst\<^sub>L\<rbrakk>\<^sub>\<sim> \<sigma>); (unrest_usubst \<lbrakk> tr ;\<^sub>L snd\<^sub>L\<rbrakk>\<^sub>\<sim> \<sigma>) \<rbrakk> \<Longrightarrow> \<sigma> \<dagger> (R1 P) = R1(\<sigma> \<dagger> P)"
+lemma subst_R1:
+  assumes "$tr\<^sup>< \<sharp>\<^sub>s \<sigma>" "$tr\<^sup>> \<sharp>\<^sub>s \<sigma>"
+  shows   "\<sigma> \<dagger> (R1 P) = R1(\<sigma> \<dagger> P)"
 proof -
-  assume "unrest_usubst \<lbrakk>tr ;\<^sub>L fst\<^sub>L\<rbrakk>\<^sub>\<sim> \<sigma>"
-  hence 1: "\<langle>\<sigma>\<rangle>\<^sub>s (tr ;\<^sub>L fst\<^sub>L) = var (tr ;\<^sub>L fst\<^sub>L)"
-    using comp_vwb_lens fst_vwb_lens tr_vwb_lens usubst_apply_unrest by blast
-  assume "unrest_usubst \<lbrakk>tr ;\<^sub>L snd\<^sub>L\<rbrakk>\<^sub>\<sim> \<sigma>"
-  hence 2: "\<langle>\<sigma>\<rangle>\<^sub>s (tr ;\<^sub>L snd\<^sub>L) = var (tr ;\<^sub>L snd\<^sub>L)"
-    using comp_vwb_lens snd_vwb_lens tr_vwb_lens usubst_apply_unrest by blast
-  show ?thesis
-    using 1 2 by (pred_auto)
+  have "\<langle>\<sigma>\<rangle>\<^sub>s (tr ;\<^sub>L fst\<^sub>L) = var (tr ;\<^sub>L fst\<^sub>L)"
+    using assms(1)
+    by (simp add: comp_vwb_lens ns_alpha_def usubst_apply_unrest)
+  moreover have "\<langle>\<sigma>\<rangle>\<^sub>s (tr ;\<^sub>L snd\<^sub>L) = var (tr ;\<^sub>L snd\<^sub>L)"
+    using assms(2)
+    by (simp add: comp_vwb_lens ns_alpha_def usubst_apply_unrest)
+  ultimately show ?thesis
+    by pred_auto
+qed
+  
+lemma subst_R1_closed [closure]: "\<lbrakk> $tr\<^sup>< \<sharp>\<^sub>s \<sigma>; $tr\<^sup>> \<sharp>\<^sub>s \<sigma>; P is R1 \<rbrakk> \<Longrightarrow> \<sigma> \<dagger> P is R1"
+  by (metis Healthy_def subst_R1)
+
+
+lemma R1_by_refinement:
+  "P is R1 \<longleftrightarrow> ((tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e \<sqsubseteq> P)"
+  by (simp add: Healthy_def; pred_auto)
+
+lemma R1_trace_extension [closure]:
+  "(tr\<^sup>> \<ge> tr\<^sup>< @ e)\<^sub>e is R1"
+  by (simp add: Healthy_def; pred_auto)
+
+lemma tr_le_trans:
+  "((tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e ;; (tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e) = (tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e"
+  by (pred_auto; metis order_refl rea_vars.select_convs(2))
+
+lemma R1_seqr:
+  "R1(R1(P) ;; R1(Q)) = (R1(P) ;; R1(Q))"
+  by pred_auto
+
+lemma R1_seqr_closure [closure]:
+  assumes "(P ::('t::trace, '\<alpha>, '\<beta>) rel_rp) is R1"
+          "(Q ::('t::trace, '\<beta>, '\<gamma>) rel_rp) is R1"
+  shows "(P ;; Q) is R1"
+proof -
+  have 1: "(tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e \<sqsubseteq> P"
+    using assms by (simp add: R1_by_refinement)
+  have 2: "(tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e \<sqsubseteq> Q"
+    using assms by (simp add: R1_by_refinement)
+  have "(((tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e :: ('t::trace, '\<alpha>, '\<beta>) rel_rp);;(tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e) \<sqsubseteq> (P;;Q)" (is "?l \<sqsubseteq> ?r")
+    using 1 2 by (simp add: seqr_mono)
+  moreover have "?l = (tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e"
+    using tr_le_trans by blast
+  ultimately show ?thesis
+    by (simp add: R1_by_refinement)
 qed
 
-(* .... *)
+lemma R1_power [closure]: "P is R1 \<Longrightarrow> P\<^bold>^n is R1"
+  by (induct n, simp_all add: upred_semiring.power_Suc closure)
+
+lemma R1_true_comp [simp]: "(R1(true) ;; R1(true)) = R1(true)"
+  by (pred_auto; metis order_refl rea_vars.select_convs(2))
+
+
+lemma R1_ok'_true: "(R1(P))\<^sup>t = R1(P\<^sup>t)"
+  by pred_auto
+
+(*
+lemma R1_ok'_false: "(R1(P))\<^sup>f = R1(P\<^sup>f)"
+  by pred_auto
+*)
+
+lemma R1_ok_true: "(R1(P))\<lbrakk>true/ok\<^sup><\<rbrakk> = R1(P\<lbrakk>true/ok\<^sup><\<rbrakk>)"
+  by pred_auto
+
+lemma R1_ok_false: "(R1(P))\<lbrakk>False/ok\<^sup><\<rbrakk> = R1(P\<lbrakk>False/ok\<^sup><\<rbrakk>)"
+  by pred_auto
+
+lemma seqr_R1_true_right: "((P ;; R1(true)) \<or> P) = (P ;; (tr\<^sup>< \<le> tr\<^sup>>)\<^sub>e)"
+  by (pred_auto)
+
+lemma conj_R1_true_right: "(P;;R1(true) \<and> Q;;R1(true)) ;; R1(true) = (P;;R1(true) \<and> Q;;R1(true))"
+  apply (pred_auto)
+  using dual_order.trans apply blast
+  using dual_order.trans apply blast
+  by (metis order_refl rea_vars.select_convs(2))
+
+(* Why is this a lemma given the assumptions are not needed? *)
+lemma R1_extend_conj_unrest: "\<lbrakk> $tr\<^sup>< \<sharp> Q; $tr\<^sup>> \<sharp> Q \<rbrakk> \<Longrightarrow> R1 (P \<and> Q) = (R1 P \<and> Q)"
+  by (rule R1_extend_conj)
+
+lemma R1_extend_conj_unrest': "\<lbrakk> $tr\<^sup>< \<sharp> Q; $tr\<^sup>> \<sharp> Q \<rbrakk> \<Longrightarrow> R1 (P \<and> Q) = (P \<and> R1 Q)"
+  by (rule R1_extend_conj')
+
+lemma R1_tr'_eq_tr: "R1(tr\<^sup>> = tr\<^sup><)\<^sub>e = (tr\<^sup>> = tr\<^sup><)\<^sub>e"
+  by (pred_auto)
+
+lemma R1_tr_less_tr': "R1(tr\<^sup>< < tr\<^sup>>)\<^sub>e = (tr\<^sup>< < tr\<^sup>>)\<^sub>e"
+  by (pred_auto)
+
+lemma tr_strict_prefix_R1_closed [closure]: "(tr\<^sup>< < tr\<^sup>>)\<^sub>e is R1"
+  by (simp add: Healthy_def; pred_auto)
+
+(* Need design healthiness conditions
+lemma R1_H2_commute: "R1(H2(P)) = H2(R1(P))"
+  by (simp add: H2_split R1_def usubst, rel_auto)
+*)
 
 subsection \<open> R2: No dependence upon trace history \<close>
 
