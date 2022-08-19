bea9996ab032fc518efc85fb0c948a85eba33cc2
Thomas Wright <tom.tdw@gmail.com>
2022-08-19 21:48:51 +0100

    Add main reactive meta-theory

--- utp_reactive.thy
+++ utp_reactive.thy
@@ -1,25 +1,14 @@
+section \<open> Meta-theory for Generalised Reactive Processes \<close>
+
 theory utp_reactive
-  imports utp_designs
-begin
-
-alphabet 'e rea_vars = des_vars +
-  wait :: bool
-  tr   :: "'e list"
-
-declare des_vars.splits [alpha_splits del]
-declare des_vars.splits [alpha_splits]
-
-definition [pred]: "R1(P) = ((tr\<^sup>< \<le> tr\<^sup>>)\<^sub>u \<and> P)"
-
-definition R2 :: "_ set \<Rightarrow> _" where
-[pred]: "R2(P) = P\<lbrakk>[],tr\<^sup><-tr\<^sup>>/tr\<^sup><,tr\<^sup>>\<rbrakk>"
-
-definition [pred]: "R3(P) = (II \<lhd> wait\<^sup>< \<rhd> P)"
-
-lemma "R1(R1(P)) = R1(P)"
-  by (pred_auto)
-
-lemma "(tr\<^sup>< \<le> tr\<^sup>>)\<^sub>u \<^bold>; (tr\<^sup>< \<le> tr\<^sup>>)\<^sub>u = (tr\<^sup>< \<le> tr\<^sup>>)\<^sub>u"
-  by (rel_auto)
-
-end
+  imports
+    utp_rea_core
+    utp_rea_event
+    utp_rea_healths
+    (* utp_rea_parallel *)
+    utp_rea_rel
+    utp_rea_cond
+    utp_rea_prog
+    (* utp_rea_wp *)
+    (* utp_rea_hoare *)
+begin end
